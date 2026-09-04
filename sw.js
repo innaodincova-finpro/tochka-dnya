09fc41c4a1e398fe6a37a6d8fc5e75b1e73b9007
innaodincova-finpro <fyzmymh5yp@privaterelay.appleid.com>
2026-09-04 22:16:32 +0300

    Исправить ярлык и полноэкранный вид на iPhone
    Новые versioned-иконки и оформление системной строки iOS в палитре приложения.

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@ const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './icon-180.png',
-  './icon-192.png',
-  './icon-512.png'
+  './icon-180-v2.png',
+  './icon-192-v2.png',
+  './icon-512-v2.png'
 ];
 
 self.addEventListener('install', event => {
