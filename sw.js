f18c22afc6e4ccaf8b4a35f26eade31facb8a387
innaodincova-finpro <fyzmymh5yp@privaterelay.appleid.com>
2026-09-08 08:56:18 +0300

    Distinguish cloud save and file backup, display confirmed time (5.8.4)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tochka-dnya-v5.8.3';
+const CACHE = 'tochka-dnya-v5.8.4';
 const FONTS = 'tochka-dnya-fonts-v1';
 const FONT_HOSTS = ['fonts.googleapis.com', 'fonts.gstatic.com'];
 const APP_SHELL = [
