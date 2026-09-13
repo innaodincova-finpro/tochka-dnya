019aeafa5ff1eab9caef4b9892ee96aba92a69dd
innaodincova-finpro <fyzmymh5yp@privaterelay.appleid.com>
2026-09-13 15:21:06 +0300

    Fix mobile document card layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tochka-dnya-v5.9.1-documents-button';
+const CACHE = 'tochka-dnya-v5.9.2-documents-card';
 const FONTS = 'tochka-dnya-fonts-v1';
 const FONT_HOSTS = ['fonts.googleapis.com', 'fonts.gstatic.com'];
 const APP_SHELL = [
