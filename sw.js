79622e3c97174fba0c5c290d893b2534452596fb
innaodincova-finpro <fyzmymh5yp@privaterelay.appleid.com>
2026-09-14 21:42:28 +0300

    Этап 1 аудита: защита дат (sw.js)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tochka-dnya-v5.9.6-third-theme';
+const CACHE = 'tochka-dnya-v5.9.7-invalid-date-safety';
 const FONTS = 'tochka-dnya-fonts-v1';
 const FONT_HOSTS = ['fonts.googleapis.com', 'fonts.gstatic.com'];
 const APP_SHELL = [
