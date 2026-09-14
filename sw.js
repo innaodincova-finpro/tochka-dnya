6517f5153fddd00118532972d2afdc3747e4f2fa
innaodincova-finpro <fyzmymh5yp@privaterelay.appleid.com>
2026-09-14 16:01:16 +0300

    5.9.4: различать записи в календаре цветом

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tochka-dnya-v5.9.3-documents-list';
+const CACHE = 'tochka-dnya-v5.9.4-calendar-colors';
 const FONTS = 'tochka-dnya-fonts-v1';
 const FONT_HOSTS = ['fonts.googleapis.com', 'fonts.gstatic.com'];
 const APP_SHELL = [
